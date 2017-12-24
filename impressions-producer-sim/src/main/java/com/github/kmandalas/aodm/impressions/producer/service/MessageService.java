a404866790fbd9a9435add54d04cfbc9d3387dcf
Kyriakos Mandalas <Kyriakos.Mandalas@IRIworldwide.com>
2017-12-24 22:56:30 +0200

    removed unnecessary folders

--- impressions-producer-sim/src/main/java/com/github/kmandalas/aodm/impressions/producer/service/MessageService.java
+++
@@ -1,9 +0,0 @@
-package com.github.kmandalas.aodm.impressions.producer.service;
-
-import com.github.kmandalas.aodm.impressions.producer.domain.Impression;
-
-public interface MessageService {
-
-	void save(Impression message) throws Exception;
-
-}
